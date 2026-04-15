a3a840a5334fd5812fa0cf4a897420b5ceb291da
Jonathan Haas <jonathan@haas.holdings>
2026-04-15 07:37:12 -0700

    Handle DeliverNew explicitly in consumer switch

--- natsbus/consumer.go
+++ natsbus/consumer.go
@@ -203,6 +203,8 @@ func (opts ConsumerOptions) subscribeOptions() []nats.SubOpt {
 		options = append(options, nats.DeliverAll())
 	case DeliverLast:
 		options = append(options, nats.DeliverLast())
+	case DeliverNew:
+		options = append(options, nats.DeliverNew())
 	default:
 		options = append(options, nats.DeliverNew())
 	}
